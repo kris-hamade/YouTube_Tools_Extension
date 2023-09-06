38b8c946503ddd1c754c4b436dffee6f944c6d90
Kris Hamade <kris.hamade@hamy.dev>
2023-09-06 14:03:00 -0400

    Updated Application for New YouTube Changes

--- js/content.js
+++ js/content.js
@@ -1,7 +1,20 @@
 const likeFunction = async () => {
-    let btn = document.querySelector("#top-level-buttons-computed > ytd-toggle-button-renderer:nth-child(1)");
-    if(btn && !btn.classList.contains("style-default-active"))
-        btn.click();
-}
+    const buttonXpath = '//*[@id="segmented-like-button"]/ytd-toggle-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]';
+    const buttonResult = document.evaluate(buttonXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+    const isLikedXpath = '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/div[1]/ytd-segmented-like-dislike-button-renderer/div[1]/ytd-toggle-button-renderer/yt-button-shape/button';
+    const xLikedResult = document.evaluate(isLikedXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+    const likeButton = buttonResult.singleNodeValue;
+    const likeButtonStatus = xLikedResult.singleNodeValue;
+
+    if (likeButton && likeButtonStatus) {
+        if (likeButtonStatus.getAttribute('aria-pressed') !== 'true') {
+            likeButton.click();
+        }
+    } else {
+        console.error('Either likeButton or likeButtonStatus is not found');
+    }
+};
 
 window.setInterval(likeFunction, 10000);
