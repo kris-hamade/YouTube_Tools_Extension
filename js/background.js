988977c3fd89b925530b4c325bdc124b3ba801d6
Kris Hamade <kris.hamade@hamy.dev>
2024-12-21 00:39:29 -0500

    Fixed Issue With Liker Not Liking

--- js/background.js
+++ js/background.js
@@ -39,6 +39,36 @@ function forwardMessageToContentScript(message) {
     });
 }
 
+function sendConfigToAllContentScripts() {
+    chrome.tabs.query({}, (tabs) => {
+        tabs.forEach((tab) => {
+            // Check if the tab's URL is a YouTube page
+            if (tab.url && tab.url.includes("youtube.com")) {
+                chrome.tabs.sendMessage(tab.id, { type: 'updateConfig', config: globalConfig }, () => {
+                    if (chrome.runtime.lastError) {
+                        console.error(`Error sending config to tab ${tab.id}: ${chrome.runtime.lastError.message}`);
+                    } else {
+                        debugLog(`Configuration successfully sent to YouTube tab ${tab.id}.`);
+                    }
+                });
+            }
+        });
+    });
+}
+
+async function fetchAndUpdateConfig() {
+    try {
+        const localConfig = await fetchLocalConfig();
+        debugLog('Local config loaded:', localConfig);
+        globalConfig = localConfig.config;
+        chrome.storage.local.set({ config: globalConfig });
+        sendConfigToAllContentScripts(); // Send config to all content scripts after loading local config
+    } catch (error) {
+        console.error('Error loading local config:', error);
+        Sentry.captureException(error);
+    }
+}
+
 async function fetchLocalConfig() {
     const url = chrome.runtime.getURL('../config.json');
     const response = await fetch(url);
@@ -61,3 +91,6 @@ async function fetchStoredConfig() {
         });
     });
 }
+
+fetchAndUpdateConfig(); // Fetch and update configuration on startup
+setInterval(fetchAndUpdateConfig, 60000); // Check for updates periodically
